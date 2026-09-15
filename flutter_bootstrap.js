4dcdd74835fdb46e176b16688a7c155ea9486a89
monkeycode-ai <monkeycode-ai@chaitin.com>
2026-09-15 23:24:29 +0000

    chore: 部署 v1.0.15 Web 构建

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "127165310" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2001931928" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
